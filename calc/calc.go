ac30db3e735488a8503224d9fb1a9812dab818c8
Easton Gibson <easton@smarty.com>
2024-11-05 10:30:10 -0700

    Add the cool stuff.

--- calc/calc.go
+++ calc/calc.go
@@ -1,7 +1,22 @@
 package calc
 
 type Addition struct{}
+type Subtract struct{}
+type Multiply struct{}
+type Division struct{}
 
 func (this Addition) Calculate(a, b int) int {
 	return a + b
+}
+
+func (this Subtract) Calculate(a, b int) int {
+	return a - b
+}
+
+func (this Multiply) Calculate(a, b int) int {
+	return a * b
+}
+
+func (this Division) Calculate(a, b int) int {
+	return a / b
 }
--- calc/calc_test.go
+++ calc/calc_test.go
@@ -28,3 +28,90 @@ func TestAddition_Calculate(t *testing.T) {
 		})
 	}
 }
+
+func TestSubtract_Calculate(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"empty", args{}, 0},
+		{"positive", args{5, 3}, 2},
+		{"negative", args{3, 7}, -4},
+		{"sub zero", args{3, 0}, 3},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			this := Subtract{}
+			if got := this.Calculate(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMultiply_Calculate(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"empty", args{}, 0},
+		{"positive", args{1, 3}, 3},
+		{"negative", args{-1, 3}, -3},
+		{"zero", args{5, 0}, 0},
+		{"normal", args{5, 4}, 20},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			this := Multiply{}
+			if got := this.Calculate(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDivision_Calculate(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"empty almost", args{0, 1}, 0},
+		{"no remainder", args{25, 5}, 5},
+		{"integer division", args{25, 4}, 6},
+		{"negative", args{4, -3}, -1},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			this := Division{}
+			if got := this.Calculate(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Calculate() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestDivision_ByZeroPanics(t *testing.T) {
+	division := &Division{}
+	defer func() {
+		r := recover()
+		if r == nil {
+			t.Fatalf("Division.ByZero did not panic")
+		}
+	}()
+	division.Calculate(1, 0)
+}
